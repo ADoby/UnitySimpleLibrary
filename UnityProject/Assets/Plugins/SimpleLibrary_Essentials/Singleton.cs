675897982adecaa8b3c091df4d3d9f8a4aca57f7
ADoby <zimmerlint@gmail.com>
2015-01-30 03:45:35 +0100

    1.0
    Added AssetStoreTools
    
    Released Essentials
    Working Timer, Example Scene and Scripts
    Working Singleton, Example Scene and Scripts

--- UnityProject/Assets/Plugins/SimpleLibrary_Essentials/Singleton.cs
+++ UnityProject/Assets/Plugins/SimpleLibrary_Essentials/Singleton.cs
@@ -3,23 +3,23 @@ using System.Collections;
 
 namespace SimpleLibrary
 {
-    public class Singleton<ChildType> : MonoBehaviour
+    public class Singleton<ChildType> : MonoBehaviour where ChildType : MonoBehaviour
     {
         //Never use this directly, thats why its private NOT protected
-        private static Singleton<ChildType> instance = null;
+        private static ChildType instance = null;
 
         //Use this to get the current single instance of this type
-        public static Singleton<ChildType> Instance
+        public static ChildType Instance
         {
             get
             {
                 if (instance == null)
-                    instance = FindObjectOfType<Singleton<ChildType>>();
+                    instance = FindObjectOfType<ChildType>();
                 if (instance == null)
                 {
                     Debug.Log(string.Format("There was no object of type <{0}>", typeof(ChildType)));
                 }
-                return instance;
+                return (ChildType)instance;
             }
             protected set
             {
@@ -28,7 +28,7 @@ namespace SimpleLibrary
         }
         protected virtual void Awake()
         {
-            Instance = this;
+            Instance = this as ChildType;
         }
     }
 }
--- UnityProject/Assets/Plugins/SimpleLibrary_Essentials/Timer.cs
+++ UnityProject/Assets/Plugins/SimpleLibrary_Essentials/Timer.cs
@@ -10,30 +10,33 @@ namespace SimpleLibrary
     {
         #region Editor
         public bool Foldout = false;
+        public float LerpValue = 0f;
         #endregion
 
         public enum TimerType
         {
             CONST,
+            LERP_TWO_CONSTANTS,
+            RANDOM_TWO_CONSTANTS,
+            LERP_RANDOM_FOUR_CONSTANTS,
+            LERP_CURVE,
             RANDOM_CURVE,
-            RANDOM_BETWEEN_TWO_CONSTANTS,
-            RANDOM_BETWEEN_TWO_CURVES
+            RANDOM_TWO_CURVES,
+            LERP_RANDOM_TWO_CURVES
         }
 
         public TimerType MyType;
 
         //CONST
         public float Time1 = 0f;
-
-        //RANDOM CURVE
-        public AnimationCurve Curve1 = AnimationCurve.Linear(0, 0, 1, 1);
-        public float ValueMultiplier = 1f;
-
-        //RANDOM TWO CONSTANTS
         public float Time2 = 0f;
+        public float Time3 = 0f;
+        public float Time4 = 0f;
 
-        //RANDOM TWO CRUVES
+        //CURVE
+        public AnimationCurve Curve1 = AnimationCurve.Linear(0, 0, 1, 1);
         public AnimationCurve Curve2 = AnimationCurve.Linear(0, 0, 1, 1);
+        public float ValueMultiplier = 1f;
 
         public float CurrentTimeValue = 0f;
         public float timer = 0f;
@@ -56,24 +59,45 @@ namespace SimpleLibrary
             Reset();
         }
 
-        public virtual void Reset()
+        public virtual void Reset(float lerpValue = 0f)
         {
+            float time = 0f;
+            float minTime1 = 0f, minTime2 = 0f;
+            float maxTime1 = 0f, maxTime2 = 0f;
+
             switch (MyType)
             {
                 case TimerType.CONST:
                     CurrentTimeValue = Time1;
                     break;
+                case TimerType.LERP_TWO_CONSTANTS:
+                    CurrentTimeValue = Mathf.Lerp(Time1, Time2, lerpValue);
+                    break;
+                case TimerType.RANDOM_TWO_CONSTANTS:
+                    CurrentTimeValue = Random.Range(Time1, Time2);
+                    break;
+                case TimerType.LERP_RANDOM_FOUR_CONSTANTS:
+                    CurrentTimeValue = Random.Range(Mathf.Lerp(Time1, Time2, lerpValue), Mathf.Lerp(Time3, Time4, lerpValue));
+                    break;
+                case TimerType.LERP_CURVE:
+                    CurrentTimeValue = Curve1.Evaluate(Mathf.Lerp(CurveMaxTime(ref Curve1), CurveMinTime(ref Curve1), lerpValue)) * ValueMultiplier;
+                    break;
                 case TimerType.RANDOM_CURVE:
                     CurrentTimeValue = Curve1.Evaluate(RandomCurveTime(ref Curve1)) * ValueMultiplier;
                     break;
-                case TimerType.RANDOM_BETWEEN_TWO_CONSTANTS:
-                    CurrentTimeValue = Random.Range(Time1, Time2);
+                case TimerType.LERP_RANDOM_TWO_CURVES:
+                    minTime1 = CurveMinTime(ref Curve1);
+                    maxTime1 = CurveMaxTime(ref Curve2);
+                    minTime2 = CurveMinTime(ref Curve2);
+                    maxTime2 = CurveMaxTime(ref Curve2);
+                    time = Mathf.Clamp(Mathf.Lerp(minTime1, maxTime1, lerpValue), minTime2, maxTime2);
+                    CurrentTimeValue = Random.Range(Curve1.Evaluate(time), Curve2.Evaluate(time)) * ValueMultiplier;
                     break;
-                case TimerType.RANDOM_BETWEEN_TWO_CURVES:
-                    float time = RandomCurveTime(ref Curve1);
-                    float curve2Min = CurveMinTime(ref Curve2);
-                    float curve2Max = CurveMaxTime(ref Curve2);
-                    time = Mathf.Clamp(time, curve2Min, curve2Max);
+                case TimerType.RANDOM_TWO_CURVES:
+                    time = RandomCurveTime(ref Curve1);
+                    minTime2 = CurveMinTime(ref Curve2);
+                    maxTime2 = CurveMaxTime(ref Curve2);
+                    time = Mathf.Clamp(time, minTime2, maxTime2);
                     CurrentTimeValue = Random.Range(Curve1.Evaluate(time), Curve2.Evaluate(time)) * ValueMultiplier;
                     break;
                 default:
@@ -99,14 +123,15 @@ namespace SimpleLibrary
         {
             timer = CurrentTimeValue;
         }
-
+        //Adds Time.deltaTime to timer
         public virtual bool Update()
         {
             return Add(Time.deltaTime);
         }
-        public virtual bool UpdateAutoReset()
+        //Adds Time.deltaTime to timer and resets if finished
+        public virtual bool UpdateAutoReset(float lerpValue = 0f)
         {
-            return AddAutoReset(Time.deltaTime);
+            return AddAutoReset(Time.deltaTime, lerpValue);
         }
 
         public virtual bool Add(float amount)
@@ -114,11 +139,11 @@ namespace SimpleLibrary
             timer = Mathf.Min(timer + amount, CurrentTimeValue);
             return Finished;
         }
-        public virtual bool AddAutoReset(float amount)
+        public virtual bool AddAutoReset(float amount, float lerpValue = 0f)
         {
             if (Add(amount))
             {
-                Reset();
+                Reset(lerpValue);
                 return true;
             }
             return false;
